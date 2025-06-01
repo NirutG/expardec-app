d06fe1f115d1ec9faee31f8a6a60ed73f9774e53
NirutG <nirut.system@gmail.com>
2025-06-01 22:24:30 +0700

    Fix ALLOWED_HOSTS for Render + custom domain

--- expardec/settings.py
+++ expardec/settings.py
@@ -7,7 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "expardec.onrender.com,www.expardec.com,expardec.com,127.0.0.1,localhost").split(",")
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
