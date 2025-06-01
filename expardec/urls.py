e3a7899c799ee49759114d1439cb25b439fd8d24
NirutG <nirut.system@gmail.com>
2025-06-01 22:48:08 +0700

    Fix import in urls.py for Render.com

--- expardec/urls.py
+++ expardec/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+# from . import views
+from expardec import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
